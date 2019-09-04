51f4110ae559258a11b1272aa79633618ce9d80c
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2019-09-04 20:01:56 +0900

    Setup eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  'parser': '@typescript-eslint/parser',
+  'extends': [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  'parserOptions': {
+    'sourceType': 'module',
+  },
+  'plugins': [
+    '@typescript-eslint',
+  ]
+};
